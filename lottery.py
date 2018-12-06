e637d655e5daa467869774a4360403e02866b9b3
aalapmaru <36465570+aalapmaru@users.noreply.github.com>
2018-12-06 14:14:27 -0500

    Add files via upload

---
+++ lottery.py
@@ -0,0 +1,21 @@
+lottery_numbers = {13, 21, 22, 5, 8}
+ 
+
+players = [
+    {
+        'name': 'Rolf',
+        'numbers': {1, 3, 8, 22, 21}
+    },
+    {
+        'name': 'Jose',
+        'numbers': {4, 9, 10, 12, 15}
+    }
+]
+
+name = players[0]['name']
+numbers = players[0]['numbers'].intersection(lottery_numbers)
+print('Player {name} got {amount} numbers right.'.format(name=name, amount=len(numbers)))
+ 
+name = players[1]['name']
+numbers = players[1]['numbers'].intersection(lottery_numbers)
+print('Player {name} got {amount} numbers right.'.format(name=name, amount=len(numbers)))
