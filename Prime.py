94a970bc5644b06bd6220534399961e4801f48d4
aalapmaru <36465570+aalapmaru@users.noreply.github.com>
2018-12-10 14:04:08 -0500

    Add files via upload

---
+++ Prime.py
@@ -0,0 +1,12 @@
+def check_if_prime(num):
+    for x in range(2,num):
+        if num%x==0:
+            print(num, 'equals', x,'*', num)
+            break
+
+    else:
+     print(num, 'is a prime number')
+
+for n in range(2,10):
+    check_if_prime(n)
+    
